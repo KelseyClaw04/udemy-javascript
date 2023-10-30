81a7fcb09330c8036484e306516095e0e840c2af
KelseyClaw04 <149321091+KelseyClaw04@users.noreply.github.com>
2023-10-30 09:41:50 -0400

    added starter file

---
+++ script.js
@@ -0,0 +1,121 @@
+// const firstName = 'Kelsey';
+// const job = 'service';
+// const birthYear = 1995;
+// const year = 2023;
+
+// const kelsey = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+// console.log(kelsey);
+
+// const kelseyNew = `I'm ${firstName}, a ${year - birthYear} years old, ${job}!`;
+// console.log(kelseyNew);
+
+// console.log(`just a regular string`);
+
+// console.log('string with \n\
+// multiple \n\
+// lines');
+
+// console.log(`String
+// multiple
+// lines`);
+
+// const age = 15;
+// const isOldEnough = age >= 18;
+
+// if (isOldEnough) {
+//     console.log(`Sarah can start driving license`);
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+// }
+
+// const birthYear = 2012;
+
+// let century;
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+// console.log(century);
+
+//type conversion
+// const inputYear = `1991`;
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number(`Kelsey`));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// type coercion
+// console.log(`I am ` + 23 + ` years old`);
+// console.log(`23` + `10` + `3`);
+// console.log(`23` * `2`);
+
+// let n = `1` + 1;
+// n = n - 1;
+// console.log(n);
+
+// 5 falsy values: 0, ", undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean(0));
+// console.log(Boolean(0));
+// console.log(Boolean(0));
+
+// const money = 5;
+// if (money) {
+//     console.log("Dont'spent it all;)");
+// } else {
+//     console.log("You should get a job!");
+// }
+// let height;
+// if (height) {
+//     console.log(`Yay! Hwigh is defined`);
+// } else {
+//     console.log()
+// }
+
+// const favorite = prompt("What's your favorite color");
+// console.log(favorite);
+
+// if (favorite === 23) {
+//     console.log(Cool! 23 is an amazing number)
+// }
+
+// const hasDriversLicense = true; //A
+// const hasGoodVision = true; //B
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+
+// if (hasDriversLicense && hasGoodVision) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log(`Someone else should drive...`);
+
+
+// const isTired = false; //c
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log(`Someone else should drive...`);
+// }
+// const scoreDolphins = (96 + 108 + 89) / 3; //a
+// const scoreKoalas = (88 + 91 + 110) / 3; //b
+// console.log(scoreDolphins);
+// console.log(scoreKoalas);
+// if (scoreDolphins > scoreKoalas) {
+//     console.log('Dolphins win the trophy');
+// } else if (scoreDolphins == scoreKoalas) {
+//     console.log('Both win the trophy');
+// } else {
+//     console.log('Koalas win the trophy');
+// }
