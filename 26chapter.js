caa0d9d553f28baa53f1a64c44bb0283413cfa5c
KelseyClaw04 <149321091+KelseyClaw04@users.noreply.github.com>
2023-10-30 09:36:44 -0400

    First commit

---
+++ 26chapter.js
@@ -0,0 +1,42 @@
+const day = `sunday`;
+
+switch (day) {
+    case `monday`: //day === `monday`
+        console.log(`Plan course structure`);
+        console.log(`Go to coding meetup`);
+        break;
+    case `tuesday`:
+        console.log(`Prepare theory videos`);
+        break;
+    case `wednesday`:
+    case `thursday`:
+        console.log(`Write code examples`);
+        break;
+    case `friday`:
+        console.log(`Record videos`);
+        break;
+    case `saturday`:
+    case `sunday`:
+        console.log(`Enjoy the weekend:D`);
+        break;
+    default:
+        console.log(`Not a valid day!`);
+}
+
+// Comment out 1st part to run 2nd part 
+
+const day = `sunday`;
+
+if (day === `monday`) {
+    console.log(`Plan course structure` + ` Go to code meetup`);
+} else if (day === `tuesday`) {
+    console.log(`Prepare theory videos`);
+} else if (day === `wednesday` || day === `thursday`) {
+    console.log("Write code examples");
+} else if (day === `friday`) {
+    console.log(`Record videos`);
+} else if (day === `saturday` || day === `sunday`) {
+    console.log(`Enjoy the weekend`);
+} else {
+    console.log(`Not a valid day`);
+}
---
+++ assignments.js
@@ -0,0 +1,74 @@
+//let country = 'United States';
+//let continent = 'North America';
+//let population = 331900000;
+//console.log (country);
+//console.log(continent);
+//console.log(population);
+//first assignment section above
+//console.log(population);
+//let isIsland = false;
+//let language;
+//console.log(language);
+//console.log(typeof isIsland);
+//console.log(typeof population);
+//console.log(typeof country);
+//console.log(typeof language);
+//second assisnment section above
+//language = 'english'
+//console.log(typeof language);
+// const language = 'english';
+// console.log(language);
+// const country = 'United States';
+// console.log(country);
+// const continent = 'North America';
+// console.log(continent);
+// const isIsland = false;
+// console.log(isIsland);
+//third assignment above
+// console.log(2 / population);
+// population++;
+// console.log(population);
+// let finlandPopulation = 6000000;
+// console.log(finlandPopulation);
+// console.log(population > finlandPopulation);
+// let averageCountryPopulation = 33000000;
+// console.log(population < averageCountryPopulation);
+// const description1 = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+// console.log(description1);
+
+// const population = 13
+
+// const isAbove = population > 33
+
+// if (isAbove) {
+//     console.log(`The United States is above average`);
+// } else {
+//     console.log(`The United States population is 22 million below average`);
+// }
+// const marksBmi = 78 / (1.69 * 1.69);
+// console.log(marksBmi);
+// const johnsBMI = 95 / (1.88 * 1.88);
+// console.log(johnsBMI);
+// let markHigherBMI = marksBmi > johnsBMI;
+// console.log(markHigherBMI);
+// if (marksBmi) {
+//     console.log(`Mark's BMI is higher than John's!`);
+// } else {
+//     console.log(`John's BMI is higher than Mark's!`);
+// }
+
+// '9' - '5';
+// '19' - '13' + '17';
+// '19' - '13' + 17;
+// '123' < 57;
+// 5 + 6 + '4' + 9 - 4 - 2; 
+
+// for (let i = 50; i <= 50; i++) {
+//     console.log(`Voter number 1 is currently voting`);
+// }
+
+// numNeighbours = 2
+// if (numNeighbours == `Only 1 border!`) {
+//     else -if (numNeighbours == `More than 1 border`)
+// else numNeighbours == `No borders`;
+// }
